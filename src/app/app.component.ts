cec12fb9306d1fbacdd3323972753d85fd5fe7cf
CryptoLorn <lorn.crypto@gmail.com>
2022-05-30 14:42:23 +0300

    homework5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+
+import {AuthService} from "./services/auth.service";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<router-outlet></router-outlet>'
 })
-export class AppComponent {
-  title = 'angular-homework';
+export class AppComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+    if(this.authService.isAuthorizationUser()) {
+      this.router.navigate(['cars']);
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {LayoutComponent} from './layout/layout/layout.component';
+import {CarsComponent} from './components/cars/cars.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {HeaderComponent} from './components/header/header.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {MainInterceptor} from "./main.interceptor";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    CarsComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      multi: true,
+      useClass: MainInterceptor
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
